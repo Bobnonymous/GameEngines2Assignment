3961a8b12eceb43bc9b1c7ce5dfe674e963729cf
Robert Cole <robert-cole@hotmail.com>
2016-02-14 00:40:37 +0000

    Bugs Chasing Soldier

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass;
+
+    public float maxSpeed = 5.0f;
+    public float maxForce = 5.0f;
+
+    public bool seekEnabled;
+    public Vector3 seekTargetPosition;
+
+    public string seekTarget;
+    public GameObject target;
+
+    void Start() {
+        seekTarget = "Trooper";
+        target = GameObject.Find(seekTarget);
+    }
+
+    void OnDrawGizmos() {
+        if (seekEnabled)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawRay(transform.position, seekTargetPosition);
+        }
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.position + force);
+    }
+
+    Vector3 Seek(Vector3 target) {
+        Vector3 toTarget = target - transform.position;
+        toTarget.Normalize();
+        Vector3 desired = toTarget * maxSpeed;
+        return desired - velocity;
+    }
+
+    void Update() {
+        seekTargetPosition = target.transform.position;
+
+        force = Vector3.zero;
+
+        if (seekEnabled)
+            force = Seek(seekTargetPosition);
+
+        force = Vector3.ClampMagnitude(force, maxForce);
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+        if (velocity.magnitude > float.Epsilon)
+            transform.forward = velocity;
+    }
+}
---
+++ Assets/Scripts/RunRandom.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunRandom : MonoBehaviour {
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass;
+
+    public float maxSpeed = 5.0f;
+    public float maxForce = 5.0f;
+
+    public Vector3 randomPosition;
+
+    void Start() {
+
+    }
+
+    Vector3 RunTo(Vector3 target) {
+        Vector3 toTarget = target - transform.position;
+        toTarget.Normalize();
+        return (toTarget * maxSpeed) - velocity;
+    }
+
+    void Update()
+    {
+        force = Vector3.zero;
+
+        force = RunTo(randomPosition);
+
+        force = Vector3.ClampMagnitude(force, maxForce);
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+        if (velocity.magnitude > float.Epsilon)
+            transform.forward = velocity;
+
+        if (Vector3.Distance(randomPosition, transform.position) < 0.05f)
+            randomPosition = new Vector3(Random.Range(-50, 50), 1, Random.Range(-50, 50));
+    }
+
+}
